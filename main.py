be8d21cd7573f32c61611820bd7786944c0e93a7
HtmlCompany <48764886+HtmlCompany@users.noreply.github.com>
2023-09-17 10:26:37 +0300

    homework_6

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from pathlib import Path
+import os
 import sys
+import shutil
+
+CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
+
+TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
+               "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g")
+
+TRANS = {}
 
 CATEGORIES = {'Archives': ['.rar', '.zip', '.gz', '.tar'], 
               'Audio': ['.mp3', '.ogg', '.wav', '.amr'],
@@ -7,6 +16,18 @@ CATEGORIES = {'Archives': ['.rar', '.zip', '.gz', '.tar'],
               'Documents': ['.doc', '.docx', '.txt', '.pdf', '.xls', '.xlsx', '.pptx', '.odt', '.ods'],
               'Images': ['.jpeg', '.png', '.jpg', '.gif']}
 
+CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
+
+TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
+               "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g")
+
+TRANS = {}
+
+for c, t in zip(tuple(CYRILLIC_SYMBOLS), TRANSLATION):
+    TRANS[ord(c)] = t
+    TRANS[ord(c.upper())] = t.upper()
+
+REZERV = list(CATEGORIES.keys())
 
 def get_categories(file:Path) -> str:
     ext = file.suffix.lower()
@@ -27,6 +48,42 @@ def sort_folder(path:Path) -> None:
             category = get_categories(item)
             move_file(item, category, path)
 
+def del_empty_folders(path:Path):
+    for folder in path.glob("**/*"):
+        counter = 0
+        if not (folder.name in REZERV) and folder.is_dir():
+            for root_dir, dirs, files in os.walk(folder):
+                if (not dirs) and (not files):
+                    os.rmdir(root_dir)
+                    counter += 1
+                    if counter > 0:
+                        del_empty_folders(path)
+def rename_files(path:Path):
+    for el in path.glob("**/*"):
+        if el.is_dir():
+            for old_path in el.glob('*'):
+                if old_path.is_file():
+                    new_path = el.joinpath(normalized(old_path.stem) + old_path.suffix)
+                    os.rename(old_path, new_path)
+
+def normalized(file_name:str):
+    new_name = file_name.replace('-', '_').replace(' ', '_'
+                ).replace('$', '_').replace('~', '_').replace('@', '_'
+                ).replace('#', '_').replace('#', '_').replace('№', '_') 
+    return new_name.translate(TRANS)
+
+def unpack_arc(path:Path):
+    for arc in path.glob('**/*'):
+        if arc.name == 'Archives':
+            arc_path = arc
+            for el in arc.glob('*'):
+                if el.is_file():
+                    unpack_path = arc_path.joinpath(el.stem)
+                    if not unpack_path.exists():
+                        unpack_path.mkdir()
+                    arc_name = el
+                    shutil.unpack_archive(arc_name, unpack_path)
+
 def main() -> str:
     try:
         path = Path(sys.argv[1])
@@ -37,6 +94,14 @@ def main() -> str:
         return "No path to folder"
     
     sort_folder(path)
+
+    del_empty_folders(path)
+
+    unpack_arc(path)
+
+    rename_files(path)
+
+    
     
     return "All right"
 
